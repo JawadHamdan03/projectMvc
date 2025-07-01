55584826ecc0b0697cf4a8823f9f6e2c5485150e
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-07-01 20:06:51 +0300

    Models added and data seeding is implemented

---
+++ Migrations/20250701170504_dataSeedingMigration.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ProjectMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class dataSeedingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "new Phones", "phones" },
+                    { 2, "new and used", "Laptops" },
+                    { 3, "All", "Washing Machines" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, null, "iphone 8 plus", 1000m },
+                    { 2, 1, null, "iphone 13 pro max", 2000m },
+                    { 3, 1, null, "Smasung ultra 21", 3000m },
+                    { 4, 2, null, "Msi", 3000m },
+                    { 5, 2, null, "Lenovo", 4000m },
+                    { 6, 3, null, "Samsung", 1000m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectMVC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=JAWADHAMDAN;Database=ProjectMVC;Trusted_connection=True;TrustServerCertificate=True");
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1,Name="phones",Description="new Phones" },
+                new Category { Id = 2,Name="Laptops",Description="new and used" },
+                new Category { Id = 3,Name="Washing Machines",Description="All" }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product {Id=1,Name="iphone 8 plus",Price=1000,CategoryId=1 },
+                new Product { Id = 2, Name = "iphone 13 pro max", Price = 2000, CategoryId = 1 },
+                new Product { Id = 3, Name = "Smasung ultra 21", Price = 3000, CategoryId = 1 },
+                new Product { Id = 4, Name = "Msi", Price = 3000, CategoryId = 2 },
+                new Product { Id = 5, Name = "Lenovo", Price = 4000, CategoryId = 2 },
+                new Product { Id = 6, Name = "Samsung", Price = 1000, CategoryId = 3 }
+                
+                
+                );
+
+        }
+    }
+}
